92023aebb9fa4dc2a0d4812decebdfe80c23a74f
Adrien El Zein <adrizein@gmail.com>
2020-02-27 00:28:36 +0100

    feat(functor): map now accepts all functor types

--- lib/map.ts
+++ lib/map.ts
@@ -1,9 +1,71 @@
+import { Maybe } from "./maybe.ts";
+import { Either } from "./either.ts";
+
 /**
- * **map** applies a function to each element of the array, returns the array of results
+ * **map** applies a function to each element of a Functor, returns the same data structure containing the images of each element
  * @param fn function that will be applied on each element
- * @param xs elements that function will be applied to
- * @returns new array with results of the map
+ * @param xs elements to which the function will be applied
+ * @returns new data structure with results of the map
  */
-export default function map<Arg, Res>(fn: (x: Arg) => Res, xs: Arg[]): Res[] {
-    return xs.map(fn);
+export default function map<Arg, Res>(fn: (x: Arg) => Res, xs: Arg[]): Res[];
+export default function map<Arg, Res>(fn: (x: Arg) => Res, xs: Maybe<Arg>): Maybe<Res>;
+export default function map<Arg, Res, Right>(fn: (x: Arg) => Res, xs: Either<Arg, Right>): Either<Res, Right>;
+export default function map<Arg, Res, Index>(fn: (x: Arg) => Res, xs: Map<Index, Arg>): Map<Index, Res>;
+export default function map<Arg, Res>(fn: (x: Arg) => Res, xs: Set<Arg>): Set<Res>;
+export default function map<Arg, Res>(fn: (x: Arg) => Res, xs: Promise<Arg>): Promise<Res>;
+export default function map<Arg, Res>(fn: (x: Arg) => Res, xs: {[key: string]: Arg}): {[key: string]: Res};
+export default function map<Arg, Res, Ia extends Iterable<Arg>>(fn: (x: Arg) => Res, xs: Ia): Res[]; // Can't guarantee to return the same input type, so we return an array
+export default function map<Arg, Res, Ma extends { map(f: (x: Arg) => Res): Fb }, Fb extends { map<T>(f: (x: Res) => T): any }>(fn: (x: Arg) => Res, xs: Ma): Fb;
+export default function map(fn, xs) {
+    if (Array.isArray(xs)) {
+        return xs.map((value) => fn(value));
+    }
+
+    if (xs instanceof Promise) {
+        return xs.then(fn);
+    }
+
+    if (xs instanceof Map) {
+        const values = new Map();
+        for (const [index, value] of xs.entries()) {
+            values.set(index, fn(value));
+        }
+
+        return values;
+    }
+
+    if (xs instanceof Set) {
+        const values = new Set();
+        for (const value of xs) {
+            values.add(fn(value));
+        }
+
+        return values;
+    }
+
+    if (!!xs && (xs['map'] instanceof Function)) {
+        return xs.map(fn)
+    }
+
+    if (!!xs && (xs[Symbol.iterator] instanceof Function)) {
+        const values: any[] = [];
+        for (const value of xs) {
+            values.push(fn(value));
+        }
+
+        return values;
+    }
+
+    if (!!xs && (typeof xs === "object")) {
+        const result = {};
+        for (const key in xs) {
+            if (xs.hasOwnProperty(key) && typeof key === 'string') {
+                result[key] = fn(xs[key]);
+            }
+        }
+
+        return result;
+    }
+
+    return [];
 }
--- test/map_test.ts
+++ test/map_test.ts
@@ -1,6 +1,8 @@
-import { assertEquals } from "https://deno.land/std@v0.33.0/testing/asserts.ts"
+import { assertEquals } from "https://deno.land/std@v0.33.0/testing/asserts.ts";
 
 import map from '../lib/map.ts';
+import maybe from "../lib/maybe.ts";
+import either from "../lib/either.ts";
 
 function double(n) {
     return n * 2;
@@ -10,5 +12,30 @@ Deno.test({
     name: "map",
     fn(): void {
         assertEquals(map(double, [1, 2, 3]), [2, 4, 6]);
+        assertEquals(map(double, []), []);
+        assertEquals(map(double, null as any), []);
+        assertEquals(map(double, maybe(1)).get(), 2);
+        assertEquals(map(double, maybe()).get(), undefined);
+        assertEquals(map(double, either.left(1)).get(), 2);
+        assertEquals(map(double, either.right('hello')).get(), 'hello');
+        assertEquals(map(double, new Set([1, 2, 3])), new Set([2, 4, 6]));
+        assertEquals(map(double, new Map([['one', 1], ['two', 2], ['three', 3]])), new Map([['one', 2], ['two', 4], ['three', 6]]));
+        assertEquals(map(double, {a: 1, b: 2, 11: 12}), {a: 2, b: 4, 11: 24});
+        const customIterable: Iterable<string> = {
+            *[Symbol.iterator]() {
+                yield "hey";
+                yield "hello";
+                yield "world";
+            }
+        };
+        assertEquals(map<string, string, Iterable<string>>((s) => s.toUpperCase(), customIterable), ["HEY", "HELLO", "WORLD"]);
+
+        class CustomFunctor<T> {
+            constructor(readonly one: T, readonly two: T) {}
+            map<R>(f: (t: T) => R): CustomFunctor<R> {
+                return new CustomFunctor(f(this.one), f(this.two));
+            }
+        }
+        assertEquals(map<string, number, CustomFunctor<string>, CustomFunctor<number>>((s) => s.length, new CustomFunctor('hello', 'a')), new CustomFunctor(5, 1));
     }
-})
+});
